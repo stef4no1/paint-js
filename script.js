79d814e9a74643ce5288928e05bc7ea403ba3f95
Stefano Straga <stefanostraga2k18@gmail.com>
2022-09-23 19:33:48 -0300

    upload js

---
+++ script.js
@@ -0,0 +1,10 @@
+const canvas = document.querySelector("canvas"),
+toolBtns = document.querySelectorAll(".tool"),
+fillColor = document.querySelector("#fill-color"),
+sizeSlider = document.querySelector("#size-slider"),
+colorBtns = document.querySelectorAll(".colors .option"),
+colorPicker = document.querySelector("#color-picker"),
+clearCanvas = document.querySelector(".clear-canvas"),
+saveImg = document.querySelector(".save-img"),
+ctx = canvas.getContext("2d");
+
